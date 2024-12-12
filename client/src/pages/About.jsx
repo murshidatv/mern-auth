b7f4bb76a629caf85a04d0352c6942c7912ca93f
Murshida <murshidatv@gmail.com>
2024-12-12 15:40:43 +0530

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 function About() {
   return (
     <div>
-      About
+      About Us
+      <div className='px-4 py-12 max-w-2xl mx-auto' >
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to my Auth App!</h1>
+      <p className= 'mb-4 text-slate-800'>Lorem Ipsum is simply dummy text of the printing 
+        and typesetting industry. Lorem Ipsum 
+        has been the industry's standard dummy 
+        text ever since the 1500s, when an un
+        known printer took a galley of type 
+        and scrambled it to make a type 
+        specimen book.  <p className= 'mb-4 text-slate-800'></p>It has survived not only 
+        five centuries, but also the leap into 
+        electronic typesetting, remaining essen
+        tially unchanged. It was popularised in
+         the 1960s with the release of Letraset
+          sheets containing  <p className= 'mb-4 text-slate-800'></p>Lorem Ipsum passages
+          , and more recently with desktop publi
+          shing software like Aldus PageMaker in
+          cluding versions of Lorem Ipsum.</p>
+    </div>
     </div>
   )
 }
